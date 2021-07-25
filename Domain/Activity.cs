f1717a25ecfed1721669fc6f4ae5a75aa7ad56cf
Pankaj Kumar <pankajq72@gmail.com>
2021-07-25 14:14:45 +0530

    End of section 14

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Domain
 {
@@ -6,13 +7,12 @@ namespace Domain
     {
       public Guid Id { get; set; }  
       public string Title { get; set; }
-
       public DateTime Date { get; set; }
       public string Description { get; set; }
       public string Category { get; set; }
       public string City { get; set; }
-
       public string Venue { get; set; }
-
+      public bool IsCancelled { get; set; }
+      public ICollection<ActivityAttendee> Attendees { get; set; } = new List<ActivityAttendee>();
     }
 }
